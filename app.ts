cf87144cff1623fa732f641b2424e3e79880dd22
federico almaraz <federicoalmaraz37@gmail.com>
2023-07-20 18:04:13 -0300

    setup typescript

--- app.ts
+++ app.ts
@@ -1,8 +1,8 @@
-const express = require("express");
+import express , {Request, Response} from 'express'
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get("/", (req: Request, res: Response) => res.type('html').send(html));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
